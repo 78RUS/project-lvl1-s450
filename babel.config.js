5779d125863b362ec825da6dd461a26668611082
BestPussyTester <berezin.info@gmail.com>
2019-02-26 09:36:08 +0300

    fix some bugs

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = [ '@babel/preset-env' ];
+  const presets = [ '@babel/preset-env', {
+    targets: {
+      node: 'current',
+    },
+  }];
   const plugins = [ ];
 
   return {
