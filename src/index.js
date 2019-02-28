5b25814889af545f17eb7c13e335ec6d950b1d9b
BestPussyTester <berezin.info@gmail.com>
2019-02-28 17:09:04 +0300

    day-5

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ export const greetingBrainEven = () => {
   return userName;
 };
 
-export const randInt = (min = 1, max = 100) => Math.floor(Math.random() * (max - min)) + min;
-export const isEven = num => num % 2 === 0;
+const randInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const isEven = num => num % 2 === 0;
 
 const winCount = 3;
 const winSign = 1;
@@ -30,7 +30,7 @@ export const brainEven = () => {
       console.log(`Congratulations, ${userName}!`);
       return winSign;
     }
-    const randomNumber = randInt();
+    const randomNumber = randInt(1, 100);
     console.log(`Question: ${randomNumber}`);
     const answer = readlineSync.question('Your answer: ');
     const rightAnswer = (isEven(randomNumber)) ? 'yes' : 'no';
@@ -43,5 +43,50 @@ export const brainEven = () => {
     return loseSign;
   };
   const userName = greetingBrainEven();
+  process(0, userName);
+};
+
+export const greetingBrainCalc = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('What is the result of the expression?');
+  console.log('');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('');
+  return userName;
+};
+
+export const brainCalc = () => {
+  const process = (acc, userName) => {
+    if (acc === winCount) {
+      console.log(`Congratulations, ${userName}!`);
+      return winSign;
+    }
+    const num1 = randInt(1, 10);
+    const num2 = randInt(1, 10);
+    const genOperator = randInt(1, 4);
+
+    const operator = (genOperator === 1) ? '+'
+      : (genOperator === 2) ? '-' : '*';
+
+    console.log(`Question: ${num1} ${operator} ${num2}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    const calculateAnswer = {
+      '+': (x, y) => x + y,
+      '-': (x, y) => x - y,
+      '*': (x, y) => x * y,
+    };
+    const rightAnswer = calculateAnswer[operator](num1, num2);
+
+    if (answer === rightAnswer.toString()) {
+      console.log('Correct!');
+      return process(acc + 1, userName);
+    }
+    console.log(`'${answer}' is wrong answer ;(.Correct answer was '${rightAnswer}'.`);
+    console.log(`Let's try again, ${userName}!`);
+    return loseSign;
+  };
+  const userName = greetingBrainCalc();
   process(0, userName);
 };
